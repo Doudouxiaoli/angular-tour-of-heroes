fbd4b14a3dd3dc731efebc8dea5c172a201e1d1b
douxiaoli <douxiaoli@medlive.cn>
2020-06-24 15:48:21 +0800

    添加应用内导航

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -11,10 +11,13 @@ export class HeroService {
 
   constructor(private messageService: MessageService) {
   }
-
   getHeroes(): Observable<Hero[]> {
-    this.messageService.add("HeroService：fetched heroes")
-    //of(HEROES) 会返回一个 Observable<Hero[]>，它会发出单个值，这个值就是这些模拟英雄的数组。
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
+  }
+  getHero(id: number): Observable<Hero> {
+    this.messageService.add(`HeroService：fetched hero id=${{id}}`)
+    //of(HEROES) 会返回一个 Observable<Hero[]>，它会发出单个值，这个值就是这些模拟英雄的数组。
+    return of(HEROES.find(hero => hero.id === id));
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,7 +2,6 @@ import {Component, OnInit} from '@angular/core';
 import {Hero} from "../hero";
 //转而使用服务删除导入HEROES语句
 import {HeroService} from "../hero.service";
-import {MessageService} from "../message.service";
 
 @Component({
   selector: 'app-heroes',
@@ -10,21 +9,15 @@ import {MessageService} from "../message.service";
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes: Hero[];
   //注入HeroService
   //这个参数同时做了两件事：1. 声明了一个私有 heroService 属性，2. 把它标记为一个 HeroService 的注入点。
-  constructor(private heroService: HeroService, private messageService: MessageService) {
+  constructor(private heroService: HeroService) {
   }
 
   //在 ngOnInit 生命周期钩子中调用 getHeroes()，之后 Angular 会在构造出 HeroesComponent 的实例之后的某个合适的时机调用 ngOnInit()
   ngOnInit(): void {
     this.getHeroes()
-  }
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroService:Selected hero id=${hero.id}`)
   }
 
   getHeroes(): void {
